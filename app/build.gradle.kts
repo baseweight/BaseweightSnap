4ea4bcc9e61eb3f281274c8e52b454403170f5c8
Joe Bowser <bowserj@gmail.com>
2025-05-21 11:14:43 -0700

    It did make it, my local was bad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ai.baseweight.baseweightsnap"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
